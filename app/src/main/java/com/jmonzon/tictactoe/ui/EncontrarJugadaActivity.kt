711afb41657e5ed1a5c4f09544879354c8b767aa
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-13 18:22:27 +0200

    Creamos pantallas necesarias

---
+++ app/src/main/java/com/jmonzon/tictactoe/ui/EncontrarJugadaActivity.kt
@@ -0,0 +1,11 @@
+package com.jmonzon.tictactoe.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EncontrarJugadaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_encontrar_jugada)
+    }
+}
--- app/src/main/java/com/jmonzon/tictactoe/ui/LoginActivity.kt
+++ app/src/main/java/com/jmonzon/tictactoe/ui/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.jmonzon.tictactoe
+package com.jmonzon.tictactoe.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jmonzon.tictactoe.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }

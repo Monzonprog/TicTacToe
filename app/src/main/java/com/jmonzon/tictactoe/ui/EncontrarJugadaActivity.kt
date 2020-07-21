99b5f2978d1e8983e5ce72c265fe2148da6dea51
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-21 20:01:34 +0200

    Generamos pantalla de Registro // Creamos botón para ir a ella

--- app/src/main/java/com/jmonzon/tictactoe/ui/EncontrarJugadaActivity.kt
+++ app/src/main/java/com/jmonzon/tictactoe/ui/EncontrarJugadaActivity.kt
@@ -2,6 +2,7 @@ package com.jmonzon.tictactoe.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jmonzon.tictactoe.R
 
 class EncontrarJugadaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/jmonzon/tictactoe/ui/LoginActivity.kt
+++ app/src/main/java/com/jmonzon/tictactoe/ui/LoginActivity.kt
@@ -1,12 +1,45 @@
 package com.jmonzon.tictactoe.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.jmonzon.tictactoe.R
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.jmonzon.tictactoe.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        //Use ViewBinding
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        events()
+    }
+
+    //Declarate events in the forms
+    private fun events() {
+        binding.buttonLogin.setOnClickListener {
+            var email = binding.editTextEmail.text.toString()
+            var password = binding.editTextPassword.text.toString()
+
+            changeLoginFormVisibility(false)
+        }
+
+        binding.textViewToRegistro.setOnClickListener {
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+    private fun changeLoginFormVisibility(showForm: Boolean) {
+        binding.progressBarLogin.visibility = if (showForm) GONE else VISIBLE
+        binding.formLogin.visibility = if (showForm) VISIBLE else GONE
     }
 }

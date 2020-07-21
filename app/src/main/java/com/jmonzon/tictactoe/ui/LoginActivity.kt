28a31053ad785be5dd8790698491b5a5680d573d
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-21 20:40:16 +0200

    Refactor

--- app/src/main/java/com/jmonzon/tictactoe/ui/LoginActivity.kt
+++ app/src/main/java/com/jmonzon/tictactoe/ui/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jmonzon.tictactoe.databinding.ActivityLoginBinding
 
--- app/src/main/java/com/jmonzon/tictactoe/ui/RegistroActivity.kt
+++ app/src/main/java/com/jmonzon/tictactoe/ui/RegistroActivity.kt
@@ -2,11 +2,16 @@ package com.jmonzon.tictactoe.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jmonzon.tictactoe.R
+import com.jmonzon.tictactoe.databinding.ActivityRegistroBinding
 
 class RegistroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }

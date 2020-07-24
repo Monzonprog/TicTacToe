7c6513ac8cdc77076bd0745fe7faa13f34ec925c
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-24 22:19:18 +0200

    Creamos UI para buscar partida

--- app/src/main/java/com/jmonzon/tictactoe/ui/FindGameActivity.kt
+++ app/src/main/java/com/jmonzon/tictactoe/ui/FindGameActivity.kt
@@ -2,11 +2,21 @@ package com.jmonzon.tictactoe.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.jmonzon.tictactoe.R
+import com.jmonzon.tictactoe.databinding.ActivityFindGameBinding
 
 class FindGameActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFindGameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_find_game)
+        binding = ActivityFindGameBinding.inflate(layoutInflater)
+        val view : View = binding.root
+        setContentView(view)
+
+        binding.progressBarFindGame.isIndeterminate = true
+        binding.textViewLoading.text = getText(R.string.cargando)
     }
 }

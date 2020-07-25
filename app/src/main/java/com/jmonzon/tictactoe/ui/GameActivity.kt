9d47dca31f9f394a5ea2e726b1a1566a49e3f4d7
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-25 18:34:23 +0200

    Añadimos viewBinding a GameActivity

--- app/src/main/java/com/jmonzon/tictactoe/ui/GameActivity.kt
+++ app/src/main/java/com/jmonzon/tictactoe/ui/GameActivity.kt
@@ -1,19 +1,22 @@
 package com.jmonzon.tictactoe.ui
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import com.jmonzon.tictactoe.R
+import com.jmonzon.tictactoe.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityGameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setSupportActionBar(binding.toolbar)
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }

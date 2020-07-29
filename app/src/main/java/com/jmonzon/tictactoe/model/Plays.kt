5bb82a616aaab0cdf13c45321afa5b9507183e8e
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-29 23:01:08 +0200

    Modificamos modelo de datos de partida

--- app/src/main/java/com/jmonzon/tictactoe/model/Plays.kt
+++ app/src/main/java/com/jmonzon/tictactoe/model/Plays.kt
@@ -1,13 +1,33 @@
 package com.jmonzon.tictactoe.model
 
 import java.util.*
+import kotlin.collections.ArrayList
 
-data class Plays(
-    var playerOneId: String,
-    var playerTwoId: String,
-    var cellSelected: List<Int>,
-    var turnPlayerOne: Boolean,
-    var winnerId: String,
-    var created: Date,
-    var abandonmentId: String
-)
+class Plays {
+    var playerOneId: String? = null
+    var playerTwoId: String? = null
+    var cellSelected: MutableList<Int>? = createList()
+    var turnPlayerOne = false
+    var winnerId: String? = null
+    var created: Date? = null
+    var abandonmentId: String? = null
+
+    constructor() {}
+    constructor(playerOneId: String?) {
+        this.playerOneId = playerOneId
+        playerTwoId = ""
+        cellSelected = createList()
+        turnPlayerOne = true
+        created = Date()
+        winnerId = ""
+        abandonmentId = ""
+    }
+
+    fun createList(): ArrayList<Int>{
+        var aux = ArrayList<Int>()
+        for (i in 0..8) {
+            aux.add(0)
+        }
+        return aux
+    }
+}

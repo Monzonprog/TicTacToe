20708649b34e1a4dd7c118e82a1cd9a925a07484
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-29 23:03:00 +0200

    Creamos método para buscar partida disponible y si no existe creamos una partida nueva

--- app/src/main/java/com/jmonzon/tictactoe/ui/FindGameActivity.kt
+++ app/src/main/java/com/jmonzon/tictactoe/ui/FindGameActivity.kt
@@ -1,27 +1,35 @@
 package com.jmonzon.tictactoe.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
 import com.jmonzon.tictactoe.R
+import com.jmonzon.tictactoe.app.Constants
 import com.jmonzon.tictactoe.databinding.ActivityFindGameBinding
+import com.jmonzon.tictactoe.model.Plays
 
 class FindGameActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFindGameBinding
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var db : FirebaseFirestore
+    private lateinit var db: FirebaseFirestore
     private lateinit var firebaseUser: FirebaseUser
     private lateinit var uid: String
+    private lateinit var playId: String
+    private var listenerRegistration: ListenerRegistration? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFindGameBinding.inflate(layoutInflater)
-        val view : View = binding.root
+        val view: View = binding.root
         setContentView(view)
         initProgressBar()
         initFirebase()
@@ -36,8 +44,84 @@ class FindGameActivity : AppCompatActivity() {
     }
 
     private fun events() {
-        binding.buttonPlay.setOnClickListener { changeMenuVisibility(false)}
+        binding.buttonPlay.setOnClickListener {
+            changeMenuVisibility(false)
+            lookForFreePlay()
+
+        }
         binding.buttonRanking.setOnClickListener { }
+    }
+
+    private fun lookForFreePlay() {
+        binding.textViewLoading.text = getText(R.string.buscando_partida)
+        db.collection("plays")
+            .whereEqualTo("playerTwoId", "")
+            .get()
+            .addOnCompleteListener(this) { task ->
+                if (task.result!!.size() == 0) {
+                    //Don´t exist any play, create a new free play
+                    createNewGame()
+                } else {
+                    //We use the position 0 because is possible that we get a list
+                    val docPlay: DocumentSnapshot = task.result!!.documents[0]
+                    playId = docPlay.id
+                    val play: Plays? = docPlay.toObject(Plays::class.java)
+                    play!!.playerTwoId = uid
+                    db.collection("plays")
+                        .document(playId)
+                        .set(play)
+                        .addOnSuccessListener(this) {
+                            startGame()
+                        }
+                        .addOnFailureListener(this) { task ->
+                            changeMenuVisibility(true)
+                            Toast.makeText(this, getText(R.string.existe_error), Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                }
+            }
+    }
+
+    private fun createNewGame() {
+        binding.textViewLoading.text = getText(R.string.creando_partida)
+        val newPlays = Plays(uid)
+
+        db.collection("plays")
+            .add(newPlays)
+            .addOnSuccessListener { documentReference ->
+                playId = documentReference.id
+                waitPlayer()
+            }
+            .addOnFailureListener { exception ->
+                changeMenuVisibility(true)
+                Toast.makeText(this, getText(R.string.existe_error), Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
+
+    private fun waitPlayer() {
+        binding.textViewLoading.text = getText(R.string.esperando_jugador)
+        listenerRegistration = db.collection("plays")
+            .document(playId)
+            .addSnapshotListener { value, error ->
+                if (value!!.get("playerTwoId") != "") {
+                    binding.textViewLoading.text = getString(R.string.hay_oponente)
+                    val handler = Handler()
+                    val runnable = Runnable {
+                        startGame()
+                    }
+                    handler.postDelayed(runnable, 1500)
+                }
+            }
+    }
+
+    private fun startGame() {
+        if (listenerRegistration != null) {
+            listenerRegistration?.remove()
+        }
+        val intent = Intent(this, GameActivity::class.java)
+        intent.putExtra(Constants.EXTRA_PLAY_ID, playId)
+        startActivity(intent)
     }
 
     private fun initProgressBar() {
